12c5733df00684a4b24c9740dd14c8ccc9f67a13
Eugene Zhang <zhang.eug@gmail.com>
2024-02-17 14:42:51 -0800

    Add location tracking, comment out background tracking

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -2,14 +2,63 @@ import { StyleSheet } from "react-native";
 
 import { View } from "@/components/Themed";
 import MapView from "react-native-maps";
+import { useEffect, useState } from "react";
+// import * as TaskManager from "expo-task-manager";
+import * as Location from "expo-location";
+
+// const LOCATION_TASK_NAME = "background-location-task";
+
+// const requestPermissions = async () => {
+//   const { status: foregroundStatus } =
+//     await Location.requestForegroundPermissionsAsync();
+//   if (foregroundStatus === "granted") {
+//     const { status: backgroundStatus } =
+//       await Location.requestBackgroundPermissionsAsync();
+//     if (backgroundStatus === "granted") {
+//       await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+//         accuracy: Location.Accuracy.Balanced,
+//       });
+//     }
+//   }
+// };
 
 export default function MapScreen() {
+  const [location, setLocation] = useState<Location.LocationObject>();
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.error("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+
+    // requestPermissions();
+  }, []);
+
+  console.log("asd", JSON.stringify(location));
+
   return (
     <View style={styles.container}>
       <MapView style={styles.map} />
     </View>
   );
 }
+
+// TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+//   if (error) {
+//     // Error occurred - check `error.message` for more details.
+//     return;
+//   }
+//   if (data) {
+//     console.log("background", data);
+//     // do something with the locations captured in the background
+//   }
+// });
 
 const styles = StyleSheet.create({
   container: {
