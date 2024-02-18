8b35ed5daef47451d18d22312cb741ea5b1dfddc
Eugene Zhang <zhang.eug@gmail.com>
2024-02-17 18:30:46 -0800

    Move map files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,13 @@
 import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 import supabase from "@/utils/supabase";
 import { getLocation, getLocationName } from "../functions/location";
-import * as Location from "expo-location";
-import MapView from "react-native-maps";
+import DisplayMap from "@/components/DisplayMap";
 
 export default function HomeScreen() {
   const [name, setName] = useState("");
-  const [location, setLocation] = useState<Location.LocationObject>();
   const [locationName, setLocationName] = useState<string>();
 
   useEffect(() => {
@@ -30,7 +27,7 @@ export default function HomeScreen() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const tempLocation = await getLocation(setLocation);
+      const tempLocation = await getLocation((_) => null);
       await getLocationName(
         setLocationName,
         tempLocation?.coords.latitude!,
@@ -53,23 +50,7 @@ export default function HomeScreen() {
       ) : (
         <Text style={styles.title}>Loading...</Text>
       )}
-      {location ? (
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: location?.coords.latitude!,
-            longitude: location?.coords.longitude!,
-            latitudeDelta: 0.0222,
-            longitudeDelta: 0.0071,
-          }}
-          showsUserLocation
-          followsUserLocation
-          showsMyLocationButton
-          showsPointsOfInterest={false}
-        />
-      ) : (
-        <Text style={styles.title}>Loading...</Text>
-      )}
+      <DisplayMap width="80%" height="60%" />
     </View>
   );
 }
@@ -91,11 +72,6 @@ const styles = StyleSheet.create({
   locationTitle: {
     fontSize: 30,
     fontWeight: "bold",
-    paddingLeft: 60,
-  },
-  map: {
-    width: "80%",
-    height: "60%",
     paddingLeft: 60,
   },
 });
--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,85 +1,18 @@
 import { StyleSheet } from "react-native";
 
-import { Text, View } from "@/components/Themed";
-import MapView from "react-native-maps";
-import { useEffect, useState } from "react";
-// import * as TaskManager from "expo-task-manager";
-import * as Location from "expo-location";
-import { getLocation } from "../functions/location";
-
-// const LOCATION_TASK_NAME = "background-location-task";
-
-// const requestPermissions = async () => {
-//   const { status: foregroundStatus } =
-//     await Location.requestForegroundPermissionsAsync();
-//   if (foregroundStatus === "granted") {
-//     const { status: backgroundStatus } =
-//       await Location.requestBackgroundPermissionsAsync();
-//     if (backgroundStatus === "granted") {
-//       await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-//         accuracy: Location.Accuracy.Balanced,
-//       });
-//     }
-//   }
-// };
+import { View } from "@/components/Themed";
+import DisplayMap from "@/components/DisplayMap";
 
 export default function MapScreen() {
-  const [location, setLocation] = useState<Location.LocationObject>();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await getLocation(setLocation);
-    };
-    fetchData();
-    // requestPermissions();
-  }, []);
-
   return (
     <View style={styles.container}>
-      {location ? (
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: location?.coords.latitude!,
-            longitude: location?.coords.longitude!,
-            latitudeDelta: 0.0222,
-            longitudeDelta: 0.0071,
-          }}
-          showsUserLocation
-          followsUserLocation
-          showsMyLocationButton
-          showsPointsOfInterest={false}
-        />
-      ) : (
-        <Text style={styles.title}>Loading...</Text>
-      )}
+      <DisplayMap width="100%" height="100%" />
     </View>
   );
 }
 
-// TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
-//   if (error) {
-//     // Error occurred - check `error.message` for more details.
-//     return;
-//   }
-//   if (data) {
-//     console.log("background", data);
-//     // do something with the locations captured in the background
-//   }
-// });
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    width: "100%",
-    height: "100%",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
   },
 });
